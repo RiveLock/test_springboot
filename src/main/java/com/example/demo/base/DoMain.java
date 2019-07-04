5a496970f9db7ba299f5a4079705f4db68360f34
jixinshi <389606993@qq.com>
2019-07-04 23:06:10 +0800

    fix

--- src/main/java/com/example/demo/base/DoMain.java
+++ src/main/java/com/example/demo/base/DoMain.java
@@ -11,7 +11,6 @@ import javax.annotation.Resource;
  * Created jixinshi on 2019-07-04.
  * 用于启动服务后自动执行
  */
-
 @Component
 public class DoMain implements CommandLineRunner {
 
@@ -20,6 +19,7 @@ public class DoMain implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        // 入口
         for (ExecutorBeanType type : ExecutorBeanType.values()) {
             templateProxy.execute(type.getBean(), type.getDesc());
         }
